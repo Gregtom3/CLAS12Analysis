e1b82c8b174c9811f1e310d7b5356c0e408962f6
Gregtom3 <gamatousek@gmail.com>
2022-07-08 16:16:19 -0400

    Makejobs

--- slurm/makeJobs.sh
+++ slurm/makeJobs.sh
@@ -6,6 +6,10 @@ RGA_F18_IN="/cache/clas12/rg-a/production/recon/fall2018/torus-1/pass1/v1/dst/tr
 RGA_F18_OUT="/cache/clas12/rg-a/production/recon/fall2018/torus+1/pass1/v1/dst/train/nSidis/"
 RGA_S19_IN="/cache/clas12/rg-a/production/recon/spring2019/torus-1/pass1/v1/dst/train/nSidis/"
 
+RGA_S19_IN="/cache/clas12/rg-a/production/recon/spring2019/torus-1/pass1/v1/dst/train/nSidis/"
+
+RGA_F18_IN_BATCH="/cache/clas12/rg-a/production/recon/spring2019/torus-1/pass1/v1/dst/train/nSidis/nSidis_005032.hipo"
+
 # --------------------------
 # *-*-*-*-*-*-*-*-*-*-*-*-*-
 # *-*-*-*-*-*-*-*-*-*-*-*-*-
@@ -20,35 +24,37 @@ CLAS12Analysisdir="/work/clas12/users/gmat/CLAS12Analysis/"
 
 # Location of hipo directories for analysis
 # --------------------------------------------------------
-hipodirs=($RGA_F18_IN $RGA_F18_OUT $RGA_S19_IN)
+hipodirs=($RGA_F18_IN_BATCH)
 
 # Beam energy associated with hipo files
 # --------------------------------------------------------
-beamEs=(10.6 10.6 10.2)
+beamEs=(10.6)
 
 # Name of output directory
 # --------------------------------------------------------
 #outdir="/volatile/clas12/users/gmat/clas12analysis.sidis.data/fall2018-torus-1-v1-nSidis"
-outdir="/volatile/clas12/users/gmat/clas12analysis.sidis.data/rg-a"
-
+#outdir="/volatile/clas12/users/gmat/clas12analysis.sidis.data/rg-a"
+outdir="/volatile/clas12/users/gmat/clas12analysis.sidis.data/fall2018-torus-1-small-batch"
 # Prefix for the output files from the analysis
 # --------------------------------------------------------
-rootname="july7"
+rootname="july8"
 
-# Process macro code location
+# Code locations
 # --------------------------------------------------------
-processcodelocation="macros/dihadron_process/"
-processcodename="pipi0_process.C"
-
-# PostProcess macro code location
-# --------------------------------------------------------
-postprocesscodelocation="macros/dihadron_process/"
-postprocesscodename="pipi0_postprocess_only.C"
+processcode="macros/dihadron_process/pipi0_process.C"
+postprocesscode="macros/dihadron_process/pipi0_postprocess_only.C"
+mergecode="macros/mergeTrees.C"
+bincode="macros/dihadron_process/pipi0_binner.C"
+fitcode="macros/dihadron_process/pipi0_fitter.C"
 
 # Location of clas12root package
 # --------------------------------------------------------
 clas12rootdir="/work/clas12/users/gmat/packages/clas12root"
 
+# Job Parameters
+# --------------------------------------------------------
+memPerCPU=8000
+nCPUs=4
 
 # --------------------------
 # *-*-*-*-*-*-*-*-*-*-*-*-*-
@@ -68,9 +74,13 @@ shellSlurmDir="${outputdir}/shells"
 outputSlurmDir="${outputdir}/output"
 runJobs="${shellSlurmDir}/runJobs.sh"
 runJobsPostProcess="${shellSlurmDir}/runJobsPostProcess.sh"
-waitFile="${outputSlurmDir}/wait.sh"
-waitJob="${outputSlurmDir}/waitJob.sh"
-mergeJob="${outputSlurmDir}/mergeJob.sh"
+mergeFile="${shellSlurmDir}/merge.sh"
+mergeSlurm="${shellSlurmDir}/mergeSlurm.slurm"
+mergeFile="${shellSlurmDir}/merge.sh"
+mergeSlurm="${shellSlurmDir}/mergeSlurm.slurm"
+fitFile="${shellSlurmDir}/fit.sh"
+fitSlurm="${shellSlurmDir}/fitSlurm.slurm"
+
 
 if [ -d "$outputdir" ]; then rm -Rf $outputdir; fi
 mkdir $outputdir
@@ -93,8 +103,7 @@ do
 	echo "set CLAS12ROOT=${clas12rootdir}" >> $processFile
 	echo "cd ${outputSlurmDir}" >> $processFile
        	echo "rm ${rootname}_${i}.out" >> $processFile
-	echo "cd ${processdir}" >> $processFile    
-	echo "clas12root ${processcodename}\\(\\\"${hipofile}\\\",\\\"${outputdir}/${rootname}_${i}.root\\\",${beamE}\\)" >> $processFile   
+	echo "clas12root ${processcode}\\(\\\"${hipofile}\\\",\\\"${outputdir}/${rootname}_${i}.root\\\",${beamE}\\)" >> $processFile   
 
 	postprocessFile="${shellSlurmDir}/${rootname}_${i}_postprocess.sh"
 	touch $postprocessFile
@@ -105,8 +114,7 @@ do
 	echo "set CLAS12ROOT=${clas12rootdir}" >> $postprocessFile
 	echo "cd ${outputSlurmDir}" >> $postprocessFile
 	echo "rm ${rootname}_${i}.out" >> $postprocessFile
-	echo "cd ${postprocessdir}" >> $postprocessFile    
-	echo "clas12root ${postprocesscodename}\\(\\\"${outputdir}/${rootname}_${i}.root\\\",${beamE}\\)" >> $postprocessFile   
+	echo "clas12root ${postprocesscode}\\(\\\"${outputdir}/${rootname}_${i}.root\\\",${beamE}\\)" >> $postprocessFile   
 	i=$((i+1))
     done
 done
@@ -120,9 +128,9 @@ chmod +x $runJobs
 echo "#!/bin/bash" > $runJobs
 echo "#SBATCH --account=clas12" >> $runJobs
 echo "#SBATCH --partition=production" >> $runJobs
-echo "#SBATCH --mem-per-cpu=8000" >> $runJobs
+echo "#SBATCH --mem-per-cpu=${memPerCPU}" >> $runJobs
 echo "#SBATCH --job-name=${rootname}" >> $runJobs
-echo "#SBATCH --cpus-per-task=4" >> $runJobs
+echo "#SBATCH --cpus-per-task=${nCPUs}" >> $runJobs
 echo "#SBATCH --time=24:00:00" >> $runJobs
 echo "#SBATCH --chdir=${workdir}" >> $runJobs
 echo "#SBATCH --array=0-${i}" >> $runJobs
@@ -142,16 +150,16 @@ chmod +x $runJobsPostProcess
 echo "#!/bin/bash" > $runJobsPostProcess
 echo "#SBATCH --account=clas12" >> $runJobsPostProcess
 echo "#SBATCH --partition=production" >> $runJobsPostProcess
-echo "#SBATCH --mem-per-cpu=4000" >> $runJobsPostProcess
+echo "#SBATCH --mem-per-cpu=${memPerCPU}" >> $runJobsPostProcess
 echo "#SBATCH --job-name=${rootname}" >> $runJobsPostProcess
-echo "#SBATCH --cpus-per-task=4" >> $runJobsPostProcess
+echo "#SBATCH --cpus-per-task=${nCPUs}" >> $runJobsPostProcess
 echo "#SBATCH --time=24:00:00" >> $runJobsPostProcess
 echo "#SBATCH --chdir=${workdir}" >> $runJobsPostProcess
 echo "#SBATCH --array=0-${i}" >> $runJobsPostProcess
 echo "#SBATCH --output=${outputSlurmDir}/%x-%a.out" >> $runJobsPostProcess
 echo "#SBATCH --error=${outputSlurmDir}/%x-%a.err" >> $runJobsPostProcess    
 echo "${shellSlurmDir}/${rootname}_\${SLURM_ARRAY_TASK_ID}_postprocess.sh" >> $runJobsPostProcess
-echo "sbatch $waitJob" >> $runJobsPostProcess
+echo "sbatch $mergeSlurm" >> $runJobsPostProcess
 
 # Create a file which will wait until (int) 0 is outputted by each job
 # This signifies the end of the batch
@@ -160,69 +168,85 @@ echo "sbatch $waitJob" >> $runJobsPostProcess
 #    - ???
 #    - ???
 # --------------------------------------------------------------------
-touch $waitFile
-chmod +x $waitFile
-echo "#!/bin/bash" > $waitFile
-echo "cd $outputSlurmDir" >> $waitFile
-echo "waitOutFile=\"wait.txt\"" >> $waitFile
-echo "touch \$waitOutFile" >> $waitFile
-echo "finishedJobs=0" >> $waitFile
-echo "nJobs=$(($i+1))" >> $waitFile
-echo "lastLine=\"\"" >> $waitFile
-echo "while [ \$finishedJobs -ne \$nJobs ]" >> $waitFile
-echo "do" >> $waitFile
-echo "    finishedJobs=0" >> $waitFile
-echo "    for outputFile in ./*.out" >> $waitFile
-echo "    do" >> $waitFile
-echo "        lastLine=\$(tail -1 \$outputFile)" >> $waitFile
-echo "        if [[ \"\$lastLine\" == \"(int) 0\" ]]; then" >>  $waitFile
-echo "            finishedJobs=\$((finishedJobs+1))" >> $waitFile
-echo "        fi" >> $waitFile
-echo "    done" >> $waitFile
-echo "echo \"\$finishedJobs completed out of \$nJobs\" >> \$waitOutFile" >> $waitFile 
-echo "sleep 30" >> $waitFile
-echo "done" >> $waitFile
-echo "sbatch ${mergeJob}" >> $waitFile
+touch $mergeFile
+chmod +x $mergeFile
+echo "#!/bin/bash" > $mergeFile
+echo "cd $outputSlurmDir" >> $mergeFile
+echo "mergeOutFile=\"merge.txt\"" >> $mergeFile
+echo "touch \$mergeOutFile" >> $mergeFile
+echo "finishedJobs=0" >> $mergeFile
+echo "nJobs=$(($i+1))" >> $mergeFile
+echo "lastLine=\"\"" >> $mergeFile
+echo "while [ \$finishedJobs -ne \$nJobs ]" >> $mergeFile
+echo "do" >> $mergeFile
+echo "    finishedJobs=0" >> $mergeFile
+echo "    for outputFile in ./*.out" >> $mergeFile
+echo "    do" >> $mergeFile
+echo "        lastLine=\$(tail -1 \$outputFile)" >> $mergeFile
+echo "        if [[ \"\$lastLine\" == \"(int) 0\" ]]; then" >>  $mergeFile
+echo "            finishedJobs=\$((finishedJobs+1))" >> $mergeFile
+echo "        fi" >> $mergeFile
+echo "    done" >> $mergeFile
+echo "echo \"\$finishedJobs completed out of \$nJobs\" >> \$mergeOutFile" >> $mergeFile 
+echo "sleep 30" >> $mergeFile
+echo "done" >> $mergeFile
+echo "clas12root ${bincode}\(\\\"${outputdir}/merged_${rootname}.root\\\"\)" >> $mergeFile
+echo "${fitFile} ${outputdir}/merged_${rootname}.root.txt" >> $mergeFile
 
-# Job for running the wait script
+# Job for running the merge script
 # ------------------------------------------------
-touch $waitJob
-chmod +x $waitJob
-echo "#!/bin/bash" > $waitJob
-echo "#SBATCH --account=clas12" >> $waitJob
-echo "#SBATCH --partition=production" >> $waitJob
-echo "#SBATCH --mem-per-cpu=2000" >> $waitJob
-echo "#SBATCH --job-name=${rootname}_waitjob" >> $waitJob
-echo "#SBATCH --cpus-per-task=1" >> $waitJob
-echo "#SBATCH --time=24:00:00" >> $waitJob
-echo "#SBATCH --chdir=${workdir}" >> $waitJob
-echo "#SBATCH --output=${outputSlurmDir}/wait-%x-%a.out" >> $waitJob
-echo "#SBATCH --error=${outputSlurmDir}/wait-%x-%a.err" >> $waitJob    
-echo "${outputSlurmDir}/wait.sh" >> $waitJob
+touch $mergeSlurm
+chmod +x $mergeSlurm
+echo "#!/bin/bash" > $mergeSlurm
+echo "#SBATCH --account=clas12" >> $mergeSlurm
+echo "#SBATCH --partition=production" >> $mergeSlurm
+echo "#SBATCH --mem-per-cpu=${memPerCPU}" >> $mergeSlurm
+echo "#SBATCH --job-name=${rootname}_mergejob" >> $mergeSlurm
+echo "#SBATCH --cpus-per-task=${nCPUs}" >> $mergeSlurm
+echo "#SBATCH --time=24:00:00" >> $mergeSlurm
+echo "#SBATCH --chdir=${workdir}" >> $mergeSlurm
+echo "#SBATCH --output=${outputSlurmDir}/merge.out" >> $mergeSlurm
+echo "#SBATCH --error=${outputSlurmDir}/merge.err" >> $mergeSlurm    
+echo "${mergeFile}" >> $mergeSlurm
+
+# Script for sending a job for each bin
+# ------------------------------------------------
+touch $fitFile
+chmod +x $fitFile
+echo "#!/bin/bash" > $fitFile
+echo "j=0" >> $fitFile
+echo "while IFS= read -r line; do" >> $fitFile
+echo "    sbatch --output=${outputSlurmDir}/fit-\$line.out --error=${outputSlurmDir}/fit-\$line.err $fitSlurm \$line" >> $fitFile
+echo "    j=\$((j+1))" >> $fitFile
+echo "done" >> $fitFile
+echo "cd $outputSlurmDir" >> $fitFile
+echo "fitOutFile=\"fit.txt\"" >> $fitFile
+echo "touch \$fitOutFile" >> $fitFile
+echo "finishedJobs=0" >> $fitFile
+echo "nJobs=\$j" >> $fitFile
+echo "lastLine=\"\"" >> $fitFile
+echo "while [ \$finishedJobs -ne \$nJobs ]" >> $fitFile
+echo "do" >> $fitFile
+echo "    finishedJobs=0" >> $fitFile
+echo "    for outputFile in ./fit*.out" >> $fitFile
+echo "    do" >> $fitFile
+echo "        lastLine=\$(tail -1 \$outputFile)" >> $fitFile
+echo "        if [[ \"\$lastLine\" == \"(int) 0\" ]]; then" >>  $fitFile
+echo "            finishedJobs=\$((finishedJobs+1))" >> $fitFile
+echo "        fi" >> $fitFile
+echo "    done" >> $fitFile
+echo "echo \"\$finishedJobs completed out of \$nJobs\" >> \$fitOutFile" >> $fitFile 
+echo "sleep 30" >> $fitFile
+echo "done" >> $fitFile
 
 
-# Job for merging all the tree_postprocess' into one
-# -----------------------------------------------------
-touch $mergeJob
-chmod +x $mergeJob
 
-echo "#!/bin/bash" > $mergeJob
-echo "#SBATCH --account=clas12" >> $mergeJob
-echo "#SBATCH --partition=production" >> $mergeJob
-echo "#SBATCH --mem-per-cpu=16000" >> $mergeJob
-echo "#SBATCH --job-name=${rootname}_mergejob" >> $mergeJob
-echo "#SBATCH --cpus-per-task=4" >> $mergeJob
-echo "#SBATCH --time=24:00:00" >> $mergeJob
-echo "#SBATCH --chdir=${workdir}" >> $mergeJob
-echo "#SBATCH --output=${outputSlurmDir}/merge-%x-%a.out" >> $mergeJob
-echo "#SBATCH --error=${outputSlurmDir}/merge-%x-%a.err" >> $mergeJob    
-echo "clas12root ${workdir}/macros/mergeTrees.C\(\\\"${outputdir}/${rootname}*.root\\\",\\\"${outputdir}/merged_${rootname}.root\\\"\)" >> $mergeJob
 
 echo "Submitting analysis jobs for the selected HIPO files"
 echo " "
 sbatch $runJobs
 echo "----------------------------------------------------"
-echo "Submitting wait script"
+echo "Submitting merge script"
 echo " "
-sbatch $waitJob
+sbatch $mergeSlurm
 echo "----------------------------------------------------"
