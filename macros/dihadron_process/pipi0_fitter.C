703c6d8f6d9b9e515b89a64b8e0c75e724523693
Gregtom3 <gamatousek@gmail.com>
2022-07-08 16:02:41 -0400

    Save

---
+++ macros/dihadron_process/pipi0_fitter.C
@@ -0,0 +1,42 @@
+#pragma once
+#if ROOT_VERSION_CODE >= ROOT_VERSION(6,00,0)
+// If anybody can figure out how to have root automically search in the include
+// directory for this file, it would be GREATLY appreciated! 
+R__LOAD_LIBRARY(libbrufit.so)
+R__LOAD_LIBRARY(libclas12ana.so)
+#include "/work/clas12/users/gmat/CLAS12Analysis/include/clas12ana/Fitter.h"
+#endif
+
+using namespace std;
+
+int pipi0_fitter(
+		 const char * inputDir = "",
+		 const char * inputFile = "",
+		 const char * wildcard = ""
+)
+{
+  //---------------
+  // Load libraries
+  //---------------
+  gSystem->Load("libclas12ana.so"); 
+  
+  //-----------------------
+  // Create Fitter Object
+  //-----------------------
+  Fitter *fitter = new Fitter(Form("%s/%s",inputDir,inputFile));
+  fitter->setWildcard(wildcard);
+  fitter->printKeys();
+
+  //-----------------------
+  // Set sPlot Object
+  //-----------------------
+  sPlotObject splot;
+  splot.outDir = Form("%s/splot_%s",inputDir,wildcard);
+  splot.variable = "Mdiphoton[0.08,0.22]";
+  splot.idbranch = "fidmerge";
+  splot.signalFactoryPDF = "Gaussian::Signal(Mdiphoton, mu[0.13,0.127,0.135], sigma[0.1,0.001,0.3])";
+  splot.bgFactoryPDF = "Chebychev::BG(Mdiphoton, a0[0.1,-1,1], a1[0.1,-1,1], a2[0.1,-1,1])";
+
+  extractSWeights(splot);
+  return 0;
+}
