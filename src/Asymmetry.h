0d744c7abdcb746f3f5140479d054318ae2f5bc6
Gregtom3 <gamatousek@gmail.com>
2022-07-12 23:11:08 -0400

    Asymmetry

--- src/Asymmetry.h
+++ src/Asymmetry.h
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-typedef std::vector<char *> charVect;
+typedef std::vector<std::string> strVect;
 
 // Enumeration for fitting procedures
 enum Method{
@@ -16,12 +16,13 @@ enum Method{
   splot = 1,
   cows = 2;
 };
+
 // Structure for each unique binning
 // std::vector's account for multidimensional binnings
 struct asymBin{
   const char * dirpath;
   Method method;
-  charVect binVar;
+  strVect binVar;
   std::vector<float> binMin;
   std::vector<float> binMax;
 };
@@ -29,17 +30,24 @@ struct asymBin{
 class Asymmetry{
  public:
   Asymmetry(const char *);
-
+  void print();
  protected:
-  charVect splitString(char *, const char *);
-  bool charVectorContains(charVect, const char *);
-  std::string charVectorFind(charVect, const char *, int);
-  int charVectorIndex(charVect, const char *);
-  bool setBin(charVect, asymBin&);
+  strVect splitString(std::string, const char *);
+  bool strVectorContains(strVect, const char *);
+  std::string strVectorFind(strVect, const char *, int);
+  int strVectorIndex(strVect, const char *);
+  bool setBin(strVect, asymBin&);
+  bool strVectorCompare(strVect, strVect);
+  std::vector<strVect> getUniqueBinnings();
+  std::vector<asymBin> getAsymBinSubset(strVect);
+  
 
   const char * _inputDir;
-  charVect _subDirs;
+  TFile *_fOut;
+  strVect _subDirs;
   std::vector<asymBin> _asymBins;
   const char *_keyword = "bin";
+
+
 };
 #endif
