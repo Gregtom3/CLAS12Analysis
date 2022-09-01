0a8faeeb56ee3baab84f70c169aca8695ff481a9
Gregtom3 <gamatousek@gmail.com>
2022-09-01 13:32:01 -0400

    RGC

--- src/PostProcess.C
+++ src/PostProcess.C
@@ -4,14 +4,38 @@ int PostProcess::Init(TTree * tree_Reco, double eBeam)
 {
   _electron_beam_energy = eBeam;
   _tree_Reco = tree_Reco;
+  _tree_Reco->SetBranchAddress("event_truth", &event_truth, &b_event);
+  _tree_Reco->SetBranchAddress("Mx", &Mx, &b_Mx);
   _tree_Reco->SetBranchAddress("Q2", &Q2, &b_Q2);
   _tree_Reco->SetBranchAddress("W", &W, &b_W);
+  _tree_Reco->SetBranchAddress("evnum", &evnum, &b_evnum);
+  _tree_Reco->SetBranchAddress("helicity", &helicity, &b_helicity);
   _tree_Reco->SetBranchAddress("nParticles", &nParticles, &b_nParticles);
   _tree_Reco->SetBranchAddress("nu", &nu, &b_nu);
+  _tree_Reco->SetBranchAddress("polarization", &polarization, &b_polarization);
+  _tree_Reco->SetBranchAddress("run", &run, &b_run);
   _tree_Reco->SetBranchAddress("x", &x, &b_x);
   _tree_Reco->SetBranchAddress("y", &y, &b_y);
-  _tree_Reco->SetBranchAddress("helicity", &helicity, &b_helicity);
-  _tree_Reco->SetBranchAddress("polarization", &polarization, &b_polarization);
+  _tree_Reco->SetBranchAddress("evtgen_parentPID", &evtgen_parentPID, &b_evtgen_parentPID);
+  _tree_Reco->SetBranchAddress("evtgen_parentID", &evtgen_parentID, &b_evtgen_parentID);
+  _tree_Reco->SetBranchAddress("evtgen_status", &evtgen_status, &b_evtgen_status);
+  _tree_Reco->SetBranchAddress("evtgen_chi2", &evtgen_chi2, &b_evtgen_chi2);
+  _tree_Reco->SetBranchAddress("evtgen_beta", &evtgen_beta, &b_evtgen_beta);
+  _tree_Reco->SetBranchAddress("evtgen_pindex", &evtgen_pindex, &b_evtgen_pindex);
+  _tree_Reco->SetBranchAddress("evtgen_vt", &evtgen_vt, &b_evtgen_vt);
+  _tree_Reco->SetBranchAddress("evtgen_vz", &evtgen_vz, &b_evtgen_vz);
+  _tree_Reco->SetBranchAddress("evtgen_vy", &evtgen_vy, &b_evtgen_vy);
+  _tree_Reco->SetBranchAddress("evtgen_vx", &evtgen_vx, &b_evtgen_vx);
+  _tree_Reco->SetBranchAddress("evtgen_phi", &evtgen_phi, &b_evtgen_phi);
+  _tree_Reco->SetBranchAddress("evtgen_eta", &evtgen_eta, &b_evtgen_eta);
+  _tree_Reco->SetBranchAddress("evtgen_theta", &evtgen_theta, &b_evtgen_theta);
+  _tree_Reco->SetBranchAddress("evtgen_E", &evtgen_E, &b_evtgen_E);
+  _tree_Reco->SetBranchAddress("evtgen_p", &evtgen_p, &b_evtgen_p);
+  _tree_Reco->SetBranchAddress("evtgen_pt", &evtgen_pt, &b_evtgen_pt);
+  _tree_Reco->SetBranchAddress("evtgen_pz", &evtgen_pz, &b_evtgen_pz);
+  _tree_Reco->SetBranchAddress("evtgen_py", &evtgen_py, &b_evtgen_py);
+  _tree_Reco->SetBranchAddress("evtgen_px", &evtgen_px, &b_evtgen_px);
+  _tree_Reco->SetBranchAddress("evtgen_pid", &evtgen_pid, &b_evtgen_pid);
   _tree_Reco->SetBranchAddress("pid", &pid, &b_pid);
   _tree_Reco->SetBranchAddress("px", &px, &b_px);
   _tree_Reco->SetBranchAddress("py", &py, &b_py);
@@ -22,12 +46,55 @@ int PostProcess::Init(TTree * tree_Reco, double eBeam)
   _tree_Reco->SetBranchAddress("theta", &theta, &b_theta);
   _tree_Reco->SetBranchAddress("eta", &eta, &b_eta);
   _tree_Reco->SetBranchAddress("phi", &phi, &b_phi);
+  _tree_Reco->SetBranchAddress("vx", &vx, &b_vx);
+  _tree_Reco->SetBranchAddress("vy", &vy, &b_vy);
   _tree_Reco->SetBranchAddress("vz", &vz, &b_vz);
+  _tree_Reco->SetBranchAddress("vt", &vt, &b_vt);
   _tree_Reco->SetBranchAddress("pindex", &pindex, &b_pindex);
   _tree_Reco->SetBranchAddress("beta", &beta, &b_beta);
   _tree_Reco->SetBranchAddress("chi2", &chi2, &b_chi2);
+  _tree_Reco->SetBranchAddress("status", &status, &b_status);
   _tree_Reco->SetBranchAddress("parentID", &parentID, &b_parentID);
   _tree_Reco->SetBranchAddress("parentPID", &parentPID, &b_parentPID);
+  _tree_Reco->SetBranchAddress("traj_sector", &traj_sector, &b_traj_sector);
+  _tree_Reco->SetBranchAddress("traj_path1", &traj_path1, &b_traj_path1);
+  _tree_Reco->SetBranchAddress("traj_path2", &traj_path2, &b_traj_path2);
+  _tree_Reco->SetBranchAddress("traj_path3", &traj_path3, &b_traj_path3);
+  _tree_Reco->SetBranchAddress("traj_det1", &traj_det1, &b_traj_det1);
+  _tree_Reco->SetBranchAddress("traj_det2", &traj_det2, &b_traj_det2);
+  _tree_Reco->SetBranchAddress("traj_det3", &traj_det3, &b_traj_det3);
+  _tree_Reco->SetBranchAddress("traj_x1", &traj_x1, &b_traj_x1);
+  _tree_Reco->SetBranchAddress("traj_x2", &traj_x2, &b_traj_x2);
+  _tree_Reco->SetBranchAddress("traj_x3", &traj_x3, &b_traj_x3);
+  _tree_Reco->SetBranchAddress("traj_y1", &traj_y1, &b_traj_y1);
+  _tree_Reco->SetBranchAddress("traj_y2", &traj_y2, &b_traj_y2);
+  _tree_Reco->SetBranchAddress("traj_y3", &traj_y3, &b_traj_y3);
+  _tree_Reco->SetBranchAddress("traj_z1", &traj_z1, &b_traj_z1);
+  _tree_Reco->SetBranchAddress("traj_z2", &traj_z2, &b_traj_z2);
+  _tree_Reco->SetBranchAddress("traj_z3", &traj_z3, &b_traj_z3);
+  _tree_Reco->SetBranchAddress("cal_sector_PCAL", &cal_sector_PCAL, &b_cal_sector_PCAL);
+  _tree_Reco->SetBranchAddress("cal_sector_ECIN", &cal_sector_ECIN, &b_cal_sector_ECIN);
+  _tree_Reco->SetBranchAddress("cal_sector_ECOUT", &cal_sector_ECOUT, &b_cal_sector_ECOUT);
+  _tree_Reco->SetBranchAddress("cal_energy_PCAL", &cal_energy_PCAL, &b_cal_energy_PCAL);
+  _tree_Reco->SetBranchAddress("cal_energy_ECIN", &cal_energy_ECIN, &b_cal_energy_ECIN);
+  _tree_Reco->SetBranchAddress("cal_energy_ECOUT", &cal_energy_ECOUT, &b_cal_energy_ECOUT);
+  _tree_Reco->SetBranchAddress("cal_time_PCAL", &cal_time_PCAL, &b_cal_time_PCAL);
+  _tree_Reco->SetBranchAddress("cal_time_ECIN", &cal_time_ECIN, &b_cal_time_ECIN);
+  _tree_Reco->SetBranchAddress("cal_time_ECOUT", &cal_time_ECOUT, &b_cal_time_ECOUT);
+  _tree_Reco->SetBranchAddress("cal_path_PCAL", &cal_path_PCAL, &b_cal_path_PCAL);
+  _tree_Reco->SetBranchAddress("cal_path_ECIN", &cal_path_ECIN, &b_cal_path_ECIN);
+  _tree_Reco->SetBranchAddress("cal_path_ECOUT", &cal_path_ECOUT, &b_cal_path_ECOUT);
+  _tree_Reco->SetBranchAddress("cal_lu_PCAL", &cal_lu_PCAL, &b_cal_lu_PCAL);
+  _tree_Reco->SetBranchAddress("cal_lu_ECIN", &cal_lu_ECIN, &b_cal_lu_ECIN);
+  _tree_Reco->SetBranchAddress("cal_lu_ECOUT", &cal_lu_ECOUT, &b_cal_lu_ECOUT);
+  _tree_Reco->SetBranchAddress("cal_lv_PCAL", &cal_lv_PCAL, &b_cal_lv_PCAL);
+  _tree_Reco->SetBranchAddress("cal_lv_ECIN", &cal_lv_ECIN, &b_cal_lv_ECIN);
+  _tree_Reco->SetBranchAddress("cal_lv_ECOUT", &cal_lv_ECOUT, &b_cal_lv_ECOUT);
+  _tree_Reco->SetBranchAddress("cal_lw_PCAL", &cal_lw_PCAL, &b_cal_lw_PCAL);
+  _tree_Reco->SetBranchAddress("cal_lw_ECIN", &cal_lw_ECIN, &b_cal_lw_ECIN);
+  _tree_Reco->SetBranchAddress("cal_lw_ECOUT", &cal_lw_ECOUT, &b_cal_lw_ECOUT);
+
+
   _nentries = _tree_Reco->GetEntriesFast();
   return 0;
 }
@@ -45,6 +112,9 @@ int PostProcess::Process(TTree *_tree_postprocess){
     break;
   case PROCESS_ID::piminuspi0_MC:
     pipi0(_tree_postprocess,1);
+    break;
+  case PROCESS_ID::Inclusive:
+    inclusive(_tree_postprocess);
     break;
   case PROCESS_ID::Elastic:
     elastic(_tree_postprocess);
@@ -212,6 +282,193 @@ int PostProcess::pipi0(TTree * _tree_postprocess, int isMC){
       }
     }
   }
+  return 0;
+}
+
+
+
+int PostProcess::inclusive(TTree * _tree_postprocess){
+  double px_e;
+  double py_e;
+  double pz_e;
+  double p_e;
+  double th_e;
+  double phi_e;
+  double z_e;
+  double vz_e;
+  
+  TLorentzVector init_electron;
+  init_electron.SetPxPyPzE(0,0,sqrt(_electron_beam_energy*_electron_beam_energy - electronMass * electronMass),_electron_beam_energy);
+  TLorentzVector init_target;
+  init_target.SetPxPyPzE(0,0,0,protonMass);
+
+  _tree_postprocess->Branch("px_e",&px_e);
+  _tree_postprocess->Branch("py_e",&py_e);
+  _tree_postprocess->Branch("pz_e",&pz_e);
+  _tree_postprocess->Branch("p_e",&p_e);
+  _tree_postprocess->Branch("th_e",&th_e);
+  _tree_postprocess->Branch("phi_e",&phi_e);
+  _tree_postprocess->Branch("z_e",&z_e);
+  _tree_postprocess->Branch("vz_e",&vz_e);
+  _tree_postprocess->Branch("Q2",&Q2);
+  _tree_postprocess->Branch("x",&x);
+  _tree_postprocess->Branch("y",&y);
+  _tree_postprocess->Branch("W",&W);
+  _tree_postprocess->Branch("helicity",&helicity);
+
+  Long64_t nbytes = 0, nb = 0;
+  for (Long64_t jentry=0; jentry<_nentries;jentry++) {
+    nb = _tree_Reco->GetEntry(jentry);   nbytes += nb;
+    px_e=0;
+    py_e=0;
+    pz_e=0;
+    p_e=0;
+    th_e=0;
+    phi_e=0;
+    z_e=0;
+    vz_e=0;
+
+    TLorentzVector electron;
+    TLorentzVector q;
+    
+    for(unsigned int i = 0 ; i < pid->size() ; i++){
+      // Identify the scattered electron
+      if(pid->at(i)==11){
+	electron.SetPxPyPzE(px->at(i),py->at(i),pz->at(i),E->at(i));
+	px_e = px->at(i);
+	py_e = py->at(i);
+	pz_e = pz->at(i);
+	p_e = p->at(i);
+	th_e = theta->at(i);
+	phi_e = phi->at(i);
+	q = init_electron - electron;
+	z_e = (init_target*electron)/(init_target*q);	
+	vz_e = vz->at(i);
+	break;
+      }
+    }
+
+    _tree_postprocess->Fill();    
+  }  
+  return 0;
+}
+
+
+int PostProcess::elastic(TTree * _tree_postprocess){
+  double px_e;
+  double py_e;
+  double pz_e;
+  double p_e;
+  double th_e;
+  double phi_e;
+  double z_e;
+  double vz_e;
+  double px_p;
+  double py_p;
+  double pz_p;
+  double p_p;
+  double th_p;
+  double phi_p;
+  double z_p;
+  double vz_p;
+  double Mmissing;
+  double Ptmissing;
+  double th_p_predict;
+  double phi_p_predict;
+
+  TLorentzVector init_electron;
+  init_electron.SetPxPyPzE(0,0,sqrt(_electron_beam_energy*_electron_beam_energy - electronMass * electronMass),_electron_beam_energy);
+  TLorentzVector init_target;
+  init_target.SetPxPyPzE(0,0,0,protonMass);
+
+  _tree_postprocess->Branch("px_e",&px_e);
+  _tree_postprocess->Branch("py_e",&py_e);
+  _tree_postprocess->Branch("pz_e",&pz_e);
+  _tree_postprocess->Branch("p_e",&p_e);
+  _tree_postprocess->Branch("th_e",&th_e);
+  _tree_postprocess->Branch("phi_e",&phi_e);
+  _tree_postprocess->Branch("z_e",&z_e);
+  _tree_postprocess->Branch("vz_e",&vz_e);
+  _tree_postprocess->Branch("px_p",&px_p);
+  _tree_postprocess->Branch("py_p",&py_p);
+  _tree_postprocess->Branch("pz_p",&pz_p);
+  _tree_postprocess->Branch("p_p",&p_p);
+  _tree_postprocess->Branch("th_p",&th_p);
+  _tree_postprocess->Branch("th_p_predict",&th_p_predict);
+  _tree_postprocess->Branch("phi_p",&phi_p);
+  _tree_postprocess->Branch("phi_p_predict",&phi_p_predict);
+  _tree_postprocess->Branch("z_p",&z_p);
+  _tree_postprocess->Branch("vz_p",&vz_p);
+  _tree_postprocess->Branch("Q2",&Q2);
+  _tree_postprocess->Branch("x",&x);
+  _tree_postprocess->Branch("y",&y);
+  _tree_postprocess->Branch("W",&W);
+  _tree_postprocess->Branch("helicity",&helicity);
+  _tree_postprocess->Branch("Mmiss",&Mmissing);
+  _tree_postprocess->Branch("Ptmiss",&Ptmissing);
+
+  Long64_t nbytes = 0, nb = 0;
+  for (Long64_t jentry=0; jentry<_nentries;jentry++) {
+    nb = _tree_Reco->GetEntry(jentry);   nbytes += nb;
+    px_e=0;
+    py_e=0;
+    pz_e=0;
+    p_e=0;
+    th_e=0;
+    phi_e=0;
+    z_e=0;
+    vz_e=0;
+    px_p=0;
+    py_p=0;
+    pz_p=0;
+    p_p=0;
+    th_p=0;
+    phi_p=0;
+    z_p=0;
+    vz_p=0;
+    Mmissing = 0;
+    Ptmissing = 0;
+    th_p_predict = 0;
+    phi_p_predict = 0;
+
+    TLorentzVector electron;
+    TLorentzVector proton;
+    TLorentzVector q;
+    
+    for(unsigned int i = 0 ; i < pid->size() ; i++){
+      // Identify the scattered electron
+      if(pid->at(i)==11){
+	electron.SetPxPyPzE(px->at(i),py->at(i),pz->at(i),E->at(i));
+	px_e = px->at(i);
+	py_e = py->at(i);
+	pz_e = pz->at(i);
+	p_e = p->at(i);
+	th_e = theta->at(i);
+	phi_e = phi->at(i);
+	q = init_electron - electron;
+	z_e = (init_target*electron)/(init_target*q);	
+	vz_e = vz->at(i);
+      }
+      if(pid->at(i)==2212){
+	proton.SetPxPyPzE(px->at(i),py->at(i),pz->at(i),E->at(i));
+	px_p = px->at(i);
+	py_p = py->at(i);
+	pz_p = pz->at(i);
+	p_p = p->at(i);
+	th_p = theta->at(i);
+	phi_p = phi->at(i);
+	z_p = (init_target*proton)/(init_target*q);	
+	vz_p = vz->at(i);
+      }
+    }
+    
+    Mmissing = (init_target+init_electron-electron-proton).M();
+    Ptmissing = (init_target+init_electron-electron-proton).Pt();
+    th_p_predict = (init_target+init_electron-electron).Theta();
+    phi_p_predict = (init_target+init_electron-electron).Phi();
+
+    _tree_postprocess->Fill();    
+  }  
   return 0;
 }
 
--- src/PostProcess.h
+++ src/PostProcess.h
@@ -22,7 +22,8 @@ class PostProcess{
     piminuspi0 = 10,
     piminuspi0_MC = 11,
 
-    Elastic = 20
+    Inclusive = 20,
+    Elastic = 21
 
   };
   int Init(TTree *, double);
@@ -51,6 +52,10 @@ class PostProcess{
       _piPID=-211;
     }
 
+    else if(strcmp(method, "inclusive")==0){
+      _process_id = PROCESS_ID::Inclusive;
+    }
+
     else if(strcmp(method, "elastic")==0){
       _process_id = PROCESS_ID::Elastic;
     }
@@ -63,19 +68,44 @@ class PostProcess{
   }
 
   int pipi0(TTree *, int);
+  int inclusive(TTree *);
   int elastic(TTree *);
   
   void set_electron_beam_energy(double E){ _electron_beam_energy = E; }
 
   // private:
-  Float_t         Q2;
-  Float_t         W;
-  Float_t         nParticles;
-  Float_t         nu;
-  Float_t         x;
-  Float_t         y;
-  Float_t         helicity;
-  Float_t         polarization;
+  Int_t           event_truth=0;
+  Float_t         Mx=0;
+  Float_t         Q2=0;
+  Float_t         W=0;
+  Float_t         evnum=0;
+  Float_t         helicity=0;
+  Float_t         nParticles=0;
+  Float_t         nu=0;
+  Float_t         polarization=0;
+  Float_t         run=0;
+  Float_t         x=0;
+  Float_t         y=0;
+  vector<float>   *evtgen_parentPID=0;
+  vector<float>   *evtgen_parentID=0;
+  vector<float>   *evtgen_status=0;
+  vector<float>   *evtgen_chi2=0;
+  vector<float>   *evtgen_beta=0;
+  vector<float>   *evtgen_pindex=0;
+  vector<float>   *evtgen_vt=0;
+  vector<float>   *evtgen_vz=0;
+  vector<float>   *evtgen_vy=0;
+  vector<float>   *evtgen_vx=0;
+  vector<float>   *evtgen_phi=0;
+  vector<float>   *evtgen_eta=0;
+  vector<float>   *evtgen_theta=0;
+  vector<float>   *evtgen_E=0;
+  vector<float>   *evtgen_p=0;
+  vector<float>   *evtgen_pt=0;
+  vector<float>   *evtgen_pz=0;
+  vector<float>   *evtgen_py=0;
+  vector<float>   *evtgen_px=0;
+  vector<float>   *evtgen_pid=0;
   vector<float>   *pid=0;
   vector<float>   *px=0;
   vector<float>   *py=0;
@@ -86,38 +116,144 @@ class PostProcess{
   vector<float>   *theta=0;
   vector<float>   *eta=0;
   vector<float>   *phi=0;
+  vector<float>   *vx=0;
+  vector<float>   *vy=0;
   vector<float>   *vz=0;
+  vector<float>   *vt=0;
   vector<float>   *pindex=0;
   vector<float>   *beta=0;
   vector<float>   *chi2=0;
+  vector<float>   *status=0;
   vector<float>   *parentID=0;
-  vector<float>   *parentPID=0; // Declaration of leaf types                                                                                                                    
+  vector<float>   *parentPID=0;
+  vector<float>   *traj_sector=0;
+  vector<float>   *traj_path1=0;
+  vector<float>   *traj_path2=0;
+  vector<float>   *traj_path3=0;
+  vector<float>   *traj_det1=0;
+  vector<float>   *traj_det2=0;
+  vector<float>   *traj_det3=0;
+  vector<float>   *traj_x1=0;
+  vector<float>   *traj_x2=0;
+  vector<float>   *traj_x3=0;
+  vector<float>   *traj_y1=0;
+  vector<float>   *traj_y2=0;
+  vector<float>   *traj_y3=0;
+  vector<float>   *traj_z1=0;
+  vector<float>   *traj_z2=0;
+  vector<float>   *traj_z3=0;
+  vector<float>   *cal_sector_PCAL=0;
+  vector<float>   *cal_sector_ECIN=0;
+  vector<float>   *cal_sector_ECOUT=0;
+  vector<float>   *cal_energy_PCAL=0;
+  vector<float>   *cal_energy_ECIN=0;
+  vector<float>   *cal_energy_ECOUT=0;
+  vector<float>   *cal_time_PCAL=0;
+  vector<float>   *cal_time_ECIN=0;
+  vector<float>   *cal_time_ECOUT=0;
+  vector<float>   *cal_path_PCAL=0;
+  vector<float>   *cal_path_ECIN=0;
+  vector<float>   *cal_path_ECOUT=0;
+  vector<float>   *cal_lu_PCAL=0;
+  vector<float>   *cal_lu_ECIN=0;
+  vector<float>   *cal_lu_ECOUT=0;
+  vector<float>   *cal_lv_PCAL=0;
+  vector<float>   *cal_lv_ECIN=0;
+  vector<float>   *cal_lv_ECOUT=0;
+  vector<float>   *cal_lw_PCAL=0;
+  vector<float>   *cal_lw_ECIN=0;
+  vector<float>   *cal_lw_ECOUT=0;
+ 
   // List of branches      
-  TBranch        *b_Q2;        
-  TBranch        *b_W;
-  TBranch        *b_nParticles; 
-  TBranch        *b_nu; 
-  TBranch        *b_x;
-  TBranch        *b_y;
-  TBranch        *b_helicity;
-  TBranch        *b_polarization;
-  TBranch        *b_pid;
-  TBranch        *b_px;
-  TBranch        *b_py;
-  TBranch        *b_pz;
-  TBranch        *b_pt;
-  TBranch        *b_p; 
-  TBranch        *b_E;                                                                                                                                                    
-  TBranch        *b_theta;
-  TBranch        *b_eta;
-  TBranch        *b_phi; 
-  TBranch        *b_vz;
-  TBranch        *b_pindex;
-  TBranch        *b_beta;   
-  TBranch        *b_chi2;
-  TBranch        *b_parentID;
-  TBranch        *b_parentPID;
-
+  TBranch        *b_event;   //!
+  TBranch        *b_Mx;   //!
+  TBranch        *b_Q2;   //!
+  TBranch        *b_W;   //!
+  TBranch        *b_evnum;   //!
+  TBranch        *b_helicity;   //!
+  TBranch        *b_nParticles;   //!
+  TBranch        *b_nu;   //!
+  TBranch        *b_polarization;   //!
+  TBranch        *b_run;   //!
+  TBranch        *b_x;   //!
+  TBranch        *b_y;   //!
+  TBranch        *b_evtgen_parentPID;   //!
+  TBranch        *b_evtgen_parentID;   //!
+  TBranch        *b_evtgen_status;   //!
+  TBranch        *b_evtgen_chi2;   //!
+  TBranch        *b_evtgen_beta;   //!
+  TBranch        *b_evtgen_pindex;   //!
+  TBranch        *b_evtgen_vt;   //!
+  TBranch        *b_evtgen_vz;   //!
+  TBranch        *b_evtgen_vy;   //!
+  TBranch        *b_evtgen_vx;   //!
+  TBranch        *b_evtgen_phi;   //!
+  TBranch        *b_evtgen_eta;   //!
+  TBranch        *b_evtgen_theta;   //!
+  TBranch        *b_evtgen_E;   //!
+  TBranch        *b_evtgen_p;   //!
+  TBranch        *b_evtgen_pt;   //!
+  TBranch        *b_evtgen_pz;   //!
+  TBranch        *b_evtgen_py;   //!
+  TBranch        *b_evtgen_px;   //!
+  TBranch        *b_evtgen_pid;   //!
+  TBranch        *b_pid;   //!
+  TBranch        *b_px;   //!
+  TBranch        *b_py;   //!
+  TBranch        *b_pz;   //!
+  TBranch        *b_pt;   //!
+  TBranch        *b_p;   //!
+  TBranch        *b_E;   //!
+  TBranch        *b_theta;   //!
+  TBranch        *b_eta;   //!
+  TBranch        *b_phi;   //!
+  TBranch        *b_vx;   //!
+  TBranch        *b_vy;   //!
+  TBranch        *b_vz;   //!
+  TBranch        *b_vt;   //!
+  TBranch        *b_pindex;   //!
+  TBranch        *b_beta;   //!
+  TBranch        *b_chi2;   //!
+  TBranch        *b_status;   //!
+  TBranch        *b_parentID;   //!
+  TBranch        *b_parentPID;   //!
+  TBranch        *b_traj_sector;   //!
+  TBranch        *b_traj_path1;   //!
+  TBranch        *b_traj_path2;   //!
+  TBranch        *b_traj_path3;   //!
+  TBranch        *b_traj_det1;   //!
+  TBranch        *b_traj_det2;   //!
+  TBranch        *b_traj_det3;   //!
+  TBranch        *b_traj_x1;   //!
+  TBranch        *b_traj_x2;   //!
+  TBranch        *b_traj_x3;   //!
+  TBranch        *b_traj_y1;   //!
+  TBranch        *b_traj_y2;   //!
+  TBranch        *b_traj_y3;   //!
+  TBranch        *b_traj_z1;   //!
+  TBranch        *b_traj_z2;   //!
+  TBranch        *b_traj_z3;   //!
+  TBranch        *b_cal_sector_PCAL;   //!
+  TBranch        *b_cal_sector_ECIN;   //!
+  TBranch        *b_cal_sector_ECOUT;   //!
+  TBranch        *b_cal_energy_PCAL;   //!
+  TBranch        *b_cal_energy_ECIN;   //!
+  TBranch        *b_cal_energy_ECOUT;   //!
+  TBranch        *b_cal_time_PCAL;   //!
+  TBranch        *b_cal_time_ECIN;   //!
+  TBranch        *b_cal_time_ECOUT;   //!
+  TBranch        *b_cal_path_PCAL;   //!
+  TBranch        *b_cal_path_ECIN;   //!
+  TBranch        *b_cal_path_ECOUT;   //!
+  TBranch        *b_cal_lu_PCAL;   //!
+  TBranch        *b_cal_lu_ECIN;   //!
+  TBranch        *b_cal_lu_ECOUT;   //!
+  TBranch        *b_cal_lv_PCAL;   //!
+  TBranch        *b_cal_lv_ECIN;   //!
+  TBranch        *b_cal_lv_ECOUT;   //!
+  TBranch        *b_cal_lw_PCAL;   //!
+  TBranch        *b_cal_lw_ECIN;   //!
+  TBranch        *b_cal_lw_ECOUT;   //!
 
   TTree *_tree_Reco;
   Long64_t _nentries;
