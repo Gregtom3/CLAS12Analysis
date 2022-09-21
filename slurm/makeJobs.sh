914196b1ee447094f010e4dc0e5a77706fc00035
Gregtom3 <gamatousek@gmail.com>
2022-09-21 13:50:14 -0400

    SAVE

--- slurm/makeJobs.sh
+++ slurm/makeJobs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+hl="---------------------------------------------------------------"
 USERNAME="$USER"
 
 if [ $# -lt 3 ]; then
@@ -26,7 +26,8 @@ fi
 
 RUNGROUPS="rg-a rg-c rga rgc"
 ANAS="MC sidisdvcs gmn"
-
+nCPUs=4
+memPerCPU=4000
 
 declare -A flags
 declare -A booleans
@@ -59,6 +60,7 @@ ana=${args[1]}
 dir=${args[2]}
 
 if ! echo "$RUNGROUPS" | grep -q "$rungroup"; then
+    echo $hl
     echo "ERROR: Run group $rungroup not valid. Must use from following list [$RUNGROUPS]"
     exit 2
 else
@@ -66,6 +68,7 @@ else
 fi
 	
 if ! echo "$ANAS" | grep -q "$ana"; then
+    echo $hl
     echo "ERROR: Analysis type $ana not valid. Must use from following list [$ANAS]"
     exit 2
 fi
@@ -75,10 +78,12 @@ fi
 # ------------------------------------
 CLAS12Analysisdir=$(cd ../;pwd)
 if [[ ! -d "/volatile/clas12/users/$USERNAME/clas12analysis.sidis.data/$rungroup/$ana/" ]]; then
+    echo $hl
     echo "ERROR: Please create the directory /volatile/clas12/users/$USERNAME/clas12analysis.sidis.data/$rungroup/$ana/"
     exit 2
 fi
 if [[ ! -d "/farm_out/$USERNAME/" ]]; then
+    echo $hl
     echo "ERROR: Please create the directory /farm_out/$USERNAME/clas12analysis.sidis.data/$rungroup/$ana/"
     exit 2
 fi
@@ -88,45 +93,58 @@ farmoutdir="/farm_out/$USERNAME/clas12analysis.sidis.data/$rungroup/$ana/$dir"
 existingruns=0
 if [ -d "${volatiledir}/" ]; then
     if [ ! -z ${booleans["o"]} ] && [ ! -z ${booleans["a"]} ]; then
+	echo $hl
 	echo "You cannot use the -o (overwrite) and -a (append) flag simultaneously...Aborting..."
 	exit 2
     elif [ ! -z ${booleans["o"]} ]; then
+	echo $hl
 	echo "Are you sure you would like to overwrite $volatiledir (Y/N):"
 	read overwrite
 	if [ $overwrite == "Y" ]; then
+	    echo $hl
 	    echo "Overwriting $volatiledir ..."
 	    rm -r $volatiledir
 	    echo "Overwritten!"
+	    mkdir -p $volatiledir
 	elif [ $overwrite == "N" ]; then
+	    echo $hl
 	    echo "Overwrite cancelled...Aborting..."
 	    exit 2
 	else
+	    echo $hl
 	    echo "ERROR: Please answer Y or N...Aborting..."
 	    exit 2
 	fi
     elif [ ! -z ${booleans["a"]} ]; then
+	echo $hl
 	echo "Are you sure you would like to append to $volatiledir (Y/N):"
 	read append
 	if [ $append == "Y" ]; then
+	    echo $hl
 	    echo "Appending to $volatiledir ..."
 	    existingruns=$(ls $volatiledir | sed 's/[^0-9]//g' | sort -n)
 	elif [ $append == "N" ]; then
+	    echo $hl
 	    echo "Appending cancelled...Aborting..."
 	    exit 2
 	else
+	    echo $hl
 	    echo "ERROR: Please answer Y or N...Aborting..."
 	    exit 2
 	fi
     else
+	echo $hl
         echo "ERROR: Directory $volatiledir already exists. You must specify a new [dir] or use -o to overwrite OR -a to append...Aborting..."
         exit 2
     fi	    
 else
+    echo $hl
     echo "Making directory $volatiledir ..."
     mkdir -p $volatiledir
 fi
 
 if [[ -d "$farmoutdir" ]]; then
+    echo $hl
     echo "Found directory $farmoutdir , should we delete it (Y/N):"
     read farmoutdelete
     if [ $farmoutdelete == "Y" ]; then
@@ -155,7 +173,7 @@ mkdir $outputSlurmDir
 
 runJobs="${shellSlurmDir}/runJobs.sh"
 
-
+echo $hl
 processcode=""
 if [ $rungroup == "rga" ]; then
     macroList=${ls "${CLAS12Analysisdir}/macros/process/rg-a"}
@@ -184,6 +202,8 @@ else
     exit 2
 fi
 
+echo $hl
+echo "Grabbing hipo directory and files within"
 trainhipodir=""
 reconhipodir=""
 if [ $rungroup == "rga" ]; then
@@ -201,6 +221,7 @@ elif [ $rungroup == "rgc" ]; then
 	reconhipodir="/volatile/clas12/rg-c/production/ana_data/*/dst/recon/*/*.hipo"
     fi
 fi
+echo $hl
 
 filenum=0
 for hipo in $trainhipodir
@@ -238,7 +259,7 @@ do
 	continue
     fi
     
-    echo "Processing run ${runNumber} from hipo file $hipo"
+    echo "Creating processing script and slurm script for run ${runNumber}"
     processFile="${shellSlurmDir}/run${runNumber}.sh"
     touch $processFile
     chmod +x $processFile
@@ -253,12 +274,12 @@ EOF
 
     slurmprocessFile="${shellSlurmDir}/run${runNumber}.slurm"
     touch $slurmprocessFile
-    cat >> $slurmprocesFile <<EOF
+    cat >> $slurmprocessFile <<EOF
 #!/bin/bash
 #SBATCH --account=clas12
 #SBATCH --partition=production
 #SBATCH --mem-per-cpu=${memPerCPU}
-#SBATCH --job-name=runprocess_$rungroup_$ana_$dir
+#SBATCH --job-name=runprocess_${rungroup}_${ana}_${dir}
 #SBATCH --cpus-per-task=${nCPUs}
 #SBATCH --time=24:00:00
 #SBATCH --output=${outputSlurmDir}/%x-%a.out
@@ -269,6 +290,7 @@ EOF
     filenum=$((filenum+1))
     if [ ! -z ${flags["maxJobs"]} ]; then
 	if [ $filenum -gt "${flags[\"maxJobs\"]}" ]; then
+	    echo $hl
 	    echo "Maximum jobs reached...Skipping rest"
 	    break
 	fi
@@ -284,7 +306,9 @@ chmod +x $organizeFile
 # If rgc, run the organize_rgc.py script
 # If rga, run the merge files script
 needsOrganize=0
-if [ [ $rungroup == "rgc" ] && [ $ana != "MC" ] ]; then
+if [ $rungroup == "rgc" ] && [ $ana != "MC" ]; then
+    echo $hl
+    echo "Creating an organizing script for the RGC dataset"
     needsOrganize=1
     cat >> $organizeFile <<EOF
 #!/bin/bash
@@ -301,6 +325,8 @@ done
 /apps/python/2.7.18/bin/python ${CLAS12Analysisdir}/macros/organize_rgc.py $volatiledir/ $dir
 EOF
 elif [ $rungroup == "rga" ]; then
+    echo $hl
+    echo "Creating a merging script for the RGA dataset"
     needsOrganize=1
     cat >> $organizeFile <<EOF
 #!/bin/bash
@@ -314,12 +340,105 @@ do
     echo \$jobsLeft >> \$organizeOutFile
 sleep 30
 done 
-if [ \$1 == 0 ]; then
-    clas12root ${CLAS12Analysisdir}/macros/mergeTrees.C\(\"${volatiledir}\",\"${dir}\",\"tree_reco\"\)
-fi
+clas12root ${CLAS12Analysisdir}/macros/mergeTrees.C\(\"${volatiledir}\",\"${dir}\",\"tree_reco\"\)
 clas12root ${CLAS12Analysisdir}/macros/mergeTrees.C\(\"${volatiledir}\",\"${dir}\",\"tree_postprocess\"\)
 EOF
+    if [ $ana == "MC" ]; then
+	break
+    fi
+    echo $hl
+    echo "For RGA analysis, several analysis scripts can run after file merging...would you like any of these to be run (Y/N):"
+    read rgaAfter
+    rgaAfterMacro=0
+    if [ $rgaAfter == "Y" ]; then
+	macroList=${ls "${CLAS12Analysisdir}/macros/process/rg-a"}
+	echo "Please select one of the following analysis scripts [${macroList}]"
+	read macro
+	processcode="${CLAS12Analysisdir}/macros/process/rg-a/${macro}"
+	if echo "$macro" | grep -w -q "$macroList"; then
+	    echo "Using processing macro $macro"
+	    rgaAfterMacro=$macro
+	else
+	    echo "Analysis macro $processcode not found...Aborting..."
+	    exit 2
+	fi
+    elif [ $rgaAfter == "N" ]; then
+	echo "Not running further analysis"
+    else
+	echo "Invalid answer...Not running further analysis...Continuing..."
+    fi
+    
+    if [ $rgaAfterMacro != 0 ]; then
+	echo $hl
+	echo "Creating postprocessing scripts and slurm scripts for RGA analysis"
+	fitFile="${shellSlurmDir}/fit.sh"
+	fitSlurm="${shellSlurmDir}/fitSlurm.slurm"
+	asymSlurm="${shellSlurmDir}/asym.slurm"
+	cat >> $organizeFile <<EOF
+clas12root ${rgaAfterMacro}\(\"${volatiledir}\",\"merged_${dir}\",\"\",1\)
+${fitFile} ${volatiledir}/merged_${dir}.root.txt
+EOF
+	touch $fitFile
+	chmod +x $fitFile
+	cat >> $fitFile <<EOF
+#!/bin/bash
+j=0
+cd ${outputSlurmDir}
+rm fit*
+while IFS= read -r line; do
+    sbatch --output=${outputSlurmDir}/fit-\$line.out --error=${outputSlurmDir}/fit-\$line.err $fitSlurm \$line
+    j=\$((j+1))
+done < \$1
+fitOutFile=\"fit.txt\"
+touch \$fitOutFile
+finishedJobs=0
+nJobs=\$j
+lastLine=\"\"
+while [ \$finishedJobs -ne \$nJobs ]
+do
+    finishedJobs=0
+    for outputFile in ../output/fit*.out
+    do
+        lastLine=\$(tail -1 \$outputFile)
+        if [[ \"\$lastLine\" == \"(int) 0\" ]]; then
+            finishedJobs=\$((finishedJobs+1))
+        fi
+    done
+echo \"\$finishedJobs completed out of \$nJobs\" >> \$fitOutFile
+sleep 10
+done
+sbatch $asymSlurm
+EOF
+
+	touch $fitSlurm
+	cat >> $fitSlurm <<EOF
+#!/bin/bash
+binname=\$1
+#SBATCH --account=clas12
+#SBATCH --partition=production
+#SBATCH --mem-per-cpu=${memPerCPU}
+#SBATCH --job-name=fit_$rungroup_$ana_$dir
+#SBATCH --cpus-per-task=${nCPUs}
+#SBATCH --time=24:00:00
+clas12root ${rgaAfterMacro}\(\"${volatiledir}\",\"merged_${dir}\",\"\$binname\",2\)
+EOF
+	
+	touch $asymSlurm
+	cat >> $asymSlurm <<EOF
+#!/bin/bash
+binname=\$1
+#SBATCH --account=clas12
+#SBATCH --partition=production
+#SBATCH --mem-per-cpu=${memPerCPU}
+#SBATCH --job-name=asym_$rungroup_$ana_$dir
+#SBATCH --cpus-per-task=${nCPUs}
+#SBATCH --time=24:00:00
+clas12root ${rgaAfterMacro}\(\"${volatiledir}\",\"\",\"\",3\)
+EOF
+    fi
 fi
+
+
 
 if [ $needsOrganize == 1 ]; then
     touch $organizeSlurm
@@ -328,10 +447,34 @@ if [ $needsOrganize == 1 ]; then
 #SBATCH --account=clas12
 #SBATCH --partition=production
 #SBATCH --mem-per-cpu=${memPerCPU}
-#SBATCH --job-name=runprocess_$rungroup_$ana_$dir
+#SBATCH --job-name=merge_$rungroup_$ana_$dir
 #SBATCH --cpus-per-task=${nCPUs}
 #SBATCH --time=24:00:00
-#SBATCH --output=${outputSlurmDir}/%x-%a.out
-#SBATCH --error=${outputSlurmDir}/%x-%a.err
-$processFile
+#SBATCH --output=${outputSlurmDir}/merge-%x-%a.out
+#SBATCH --error=${outputSlurmDir}/merge-%x-%a.err
+$organizeFile
 EOF
+fi    
+
+echo $hl
+echo $hl
+echo "DONE WITH FILE CREATION"
+echo $hl
+echo $hl
+echo "Submitting processing slurm jobs"
+ls "${shellSlurmDir}/run*.slurm" | xargs sbatch -q
+n1=$(ls "${shellSlurmDir}/run*.slurm" | wc -l)
+echo "Submitted ${n1} processing slurm jobs"
+
+if [ $needsOrganize == 1 ]; then
+    echo "Submitting organzing job"
+    sbatch $organizeSlurm
+fi
+
+echo "----------------------------------------------------"
+echo "Here is how you can monitor your runs..."
+echo "----------------------------------------------------"
+echo "Location of slurm .out/.err : $outputSlurmDir"
+echo "Location of shell scripts : $shellSlurmDir"
+echo "Location of saved .root files : $volatiledir"
+echo "----------------------------------------------------"
