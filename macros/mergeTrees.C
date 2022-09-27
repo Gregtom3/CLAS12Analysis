f066c517acbdd4c8484c03f7e8ea66b299de590b
Gregtom3 <gamatousek@gmail.com>
2022-09-27 11:32:15 -0400

    Cleaning

--- macros/mergeTrees.C
+++ macros/mergeTrees.C
@@ -34,9 +34,9 @@ R__LOAD_LIBRARY(libclas12ana.so)
 
 using namespace std;
 
-int mergeTrees(const char * inputDir="/lustre19/expphy/volatile/clas12/users/gmat/clas12analysis.sidis.data/fall2018-torus-1-small-batch/july14",
-	       const char * rootname="july14",
-	       const char * treeName="tree_postprocess"
+int mergeTrees(const char * inputDir="",
+	       const char * rootname="",
+	       const char * treeName=""
 ){
   //---------------
   // Load libraries
